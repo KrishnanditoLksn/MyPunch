3e1225c69ad168b85a8112d84ea827ba9d335b37
Gorthdzx <krislaksana123@gmail.com>
2024-09-19 19:23:17 +0700

    feat: appbar title

--- app/src/main/java/app/ditodev/mypunch/MainActivity.kt
+++ app/src/main/java/app/ditodev/mypunch/MainActivity.kt
@@ -1,5 +1,7 @@
 package app.ditodev.mypunch
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +11,13 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.rgb(0,191,255)))
+        supportActionBar!!.setTitle(R.string.app_name)
     }
 }
