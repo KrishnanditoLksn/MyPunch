7a0a6f0a74910879c383041feebdb161fb263a25
Gorthdzx <krislaksana123@gmail.com>
2024-09-20 20:48:54 +0700

    feat: intent to list screen

--- app/src/main/java/app/ditodev/mypunch/MainActivity.kt
+++ app/src/main/java/app/ditodev/mypunch/MainActivity.kt
@@ -1,23 +1,40 @@
 package app.ditodev.mypunch
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import app.ditodev.mypunch.activity.OnePunchLists
+import app.ditodev.mypunch.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.rgb(16,27,131)))
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.rgb(16, 27, 131)))
         supportActionBar!!.setTitle(R.string.app_name)
+
+        binding.ivbList.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0?.id != null) {
+            when (p0.id) {
+                R.id.ivb_list -> {
+                    startActivity(Intent(this@MainActivity, OnePunchLists::class.java))
+                }
+            }
+        }
     }
 }
